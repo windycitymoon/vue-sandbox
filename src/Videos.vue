8f810efabdd3e34c63726a4fd72a4698d7f6f1ab
Derek Moon <dmoon79@gmail.com>
2017-08-15 09:50:03 -0700

    Vue Return items

--- src/Videos.vue
+++ src/Videos.vue
@@ -1,7 +1,17 @@
 <template lang="html">
   <div id="videos">
-    <p>{{ initial }}</p>
+      <div v-for="item in items" :key="item.id">
+        <p v-if="item.title">
+          <h3>{{ item.title }}</h3>
+          <a :href="'http://youtube.com' + item.url" class="href">View Video</a> <br>
+          <p>{{ item.views }}</p>
+        </p>
+      </div>
+
   </div>
+
+  <!-- <iframe width="560" height="315" :src="'http://youtube.com/embed' + item.url" frameborder="0" allowfullscreen></iframe><br/> -->
+
 </template>
 
 <script>
@@ -10,15 +20,15 @@ export default {
   name: 'videos',
   data () {
     return {
-      initial: 'Videos Loading....'
+      initial: 'Videos Loading....',
+      items: []
     }
   },
   created () {
     axios.get('src/assets/recommendations.json')
       .then( response => {
-        console.log(response);
-        // this.inital = response.data.[i].title
-        // TODO: Iterate over this response object inside the template above
+
+        this.items = response.data;
       })
       .catch( error => {
         console.log(error);
