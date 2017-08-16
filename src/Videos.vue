7f735aac430e56114bde5bbe9d7a1c9244f72e4a
Derek Moon <dmoon79@gmail.com>
2017-08-16 03:18:41 -0700

    update component

--- src/Videos.vue
+++ src/Videos.vue
@@ -1,13 +1,13 @@
 <template lang="html">
   <div id="videos">
-      <div v-for="item in items" :key="item.id">
+      <div v-for="item in items" :key="item.id" class="">
         <p v-if="item.title">
           <h3>{{ item.title }}</h3>
-          <a :href="'http://youtube.com' + item.url" class="href">View Video</a> <br>
+          <a :href="'http://youtube.com' + item.url" target="_blank">View Video</a><br>
           <p>{{ item.views }}</p>
         </p>
+        <button type="button" name="button">Open Video</button>
       </div>
-
   </div>
 
   <!-- <iframe width="560" height="315" :src="'http://youtube.com/embed' + item.url" frameborder="0" allowfullscreen></iframe><br/> -->
@@ -20,14 +20,12 @@ export default {
   name: 'videos',
   data () {
     return {
-      initial: 'Videos Loading....',
       items: []
     }
   },
   created () {
     axios.get('src/assets/recommendations.json')
       .then( response => {
-
         this.items = response.data;
       })
       .catch( error => {
